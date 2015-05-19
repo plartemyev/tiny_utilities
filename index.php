8c7e404bd1db93436d8e25635291e520e621b32e
Pavel Artemyev <pasha@prt-term2.pavel.bz>
2015-05-19 17:11:24 +0300

    Tu tuuruuu tuuuuu...

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ if(isset($_SESSION["renderCounter"])){
 else{$_SESSION["renderCounter"] = 1;}
 
 echo "<p>This page was rendered " . $_SESSION["renderCounter"] . " times in this session.</p>";
-echo "<p>My custom PHP.INI paremeters is:</p>"
+echo "<p>My custom PHP.INI paremeters is:</p>";
 echo "<p>" . $myPHPparams . "</p>";
 ?>
 
