7d2ffd78fd4ecc13d8098694241a17c79676e2dd
Pavel Artemyev <pasha@prt-term2.pavel.bz>
2015-05-19 21:47:42 +0300

    Just another try 9021

--- index.php
+++ index.php
@@ -1,7 +1,5 @@
 <?php
 session_start();
-$myPHPparams = file_get_contents("/etc/php.d/redis.ini");
-$redisout = file_get_contents("/tmp/redis_install.log");
 ?>
 
 <!DOCTYPE html>
@@ -12,11 +10,7 @@ $redisout = file_get_contents("/tmp/redis_install.log");
 if(isset($_SESSION["renderCounter"])){
   $_SESSION["renderCounter"] += 1;}
 else{$_SESSION["renderCounter"] = 1;}
-
 echo "<p>This page was rendered " . $_SESSION["renderCounter"] . " times in this session.</p>";
-echo "<p>My custom PHP.INI paremeters is:</p>";
-echo "<p>" . $myPHPparams . "</p>";
-echo "<p>" . $redisout . "</p>";
 ?>
 
 </body>
