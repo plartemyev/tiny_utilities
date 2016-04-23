c6d682c758cf6f9c5d9ed0589bb13db6c2ec5544
Pavel Artemyev <a@pavel.bz>
2016-04-23 12:15:31 +0300

    Corrected analyzer's console output.

--- lax-bandwidth-analyzer.py
+++ lax-bandwidth-analyzer.py
@@ -1,22 +1,24 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+
 import csv
 import bytes_converter
 import sys
 import numpy
 
-input = sys.argv[1]
+input_file = sys.argv[1]
 bulky_list = []
 
-with open(input) as inputcsv:
-    dialect = csv.Sniffer().sniff(inputcsv.read(130))
-    inputcsv.seek(0)
-    statreader = csv.DictReader(inputcsv, restval='', dialect=dialect)
-    for row in statreader:
+with open(input_file) as input_csv:
+    dialect = csv.Sniffer().sniff(input_csv.read(130))
+    input_csv.seek(0)
+    stat_reader = csv.DictReader(input_csv, restval='', dialect=dialect)
+    for row in stat_reader:
         bulky_list.append(bytes_converter.human2bytes(row['Total speed PEAK']))
 
 max_value = max(bulky_list)
-print('PEAK speed in bytes/s:', max_value, '\t\t\thuman readable:', bytes_converter.bytes2human(max_value))
+print('PEAK speed is: {}bit/s ({} bit/s)'
+      .format(bytes_converter.bytes2human(max_value * 8, symbols='iec_ext'), max_value * 8))
 
 percentile_99 = numpy.percentile(bulky_list, 99)
-print('99 Percentile is within bytes/s:', percentile_99, '\thuman readable:',
-      bytes_converter.bytes2human(percentile_99))
+print('99 Percentile is within: {}bit/s ({} bit/s)'
+      .format(bytes_converter.bytes2human(percentile_99 * 8, symbols='iec_ext'), percentile_99 * 8))
